1816cd14132b95334fea2be45935c30a66c35e3d
InvizHer <112791490+InvizHer@users.noreply.github.com>
2024-03-16 13:51:56 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,30 +116,18 @@ themeButton.addEventListener("click", () => {
         });
 
     /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
 
-document.addEventListener("DOMContentLoaded", function() {
-            /*=============== SCROLL REVEAL ANIMATION ===============*/
-            const sr = ScrollReveal({
-                origin: "top",
-                distance: "60px",
-                duration: 2500,
-                delay: 400,
-            });
-
-            sr.reveal(".profile__border");
-            sr.reveal(".profile__name", { delay: 500 });
-            sr.reveal(".profile__profession", { delay: 600 });
-            sr.reveal(".profile__social", { delay: 700 });
-            sr.reveal(".profile__info-group", { interval: 100, delay: 700 });
-            sr.reveal(".profile__buttons", { delay: 800 });
-            sr.reveal(".filters__content", { delay: 900 });
-            sr.reveal(".filters", { delay: 1000 });
-
-            // Show content after all animations are complete
-            sr.reveal(".filters", { delay: 1000, complete: function() {
-                document.querySelector('.main').style.opacity = '1';
-                document.querySelector('.article__container').style.opacity = '1';
-                document.querySelector('.headr').style.opacity = '1';
-            }});
-        });
-    </script>
+sr.reveal(".profile__border");
+sr.reveal(".profile__name", { delay: 500 });
+sr.reveal(".profile__profession", { delay: 600 });
+sr.reveal(".profile__social", { delay: 700 });
+sr.reveal(".profile__info-group", { interval: 100, delay: 700 });
+sr.reveal(".profile__buttons", { delay: 800 });
+sr.reveal(".filters__content", { delay: 900 });
+sr.reveal(".filters", { delay: 1000 });
